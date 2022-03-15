4b8dbd8f058f276e98d5091768565aa982405838
Glyson <kauaglyson@gmail.com>
2022-03-15 01:35:43 -0300

    Update com inputs
    Agora o usuário passará os valores para realizar a operação.

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,10 +1,8 @@
 print('**********************************')
 print('Qual operação você quer fazer? \n')
 choice = input(('1- SOMA\n2- SUBTRAÇÃO\n3- MULTIPLICAÇÃO\n4- DIVISÃO\n'))
-
-#numX = int(input("Digite o número um: "))
-#numY = int(input("Digite o número dois: "))
-numY, numX = 5, 5
+numX = float(input("Digite o número um: "))
+numY = float(input("Digite o número dois: "))
 
 
 def soma(x, y):
@@ -24,9 +22,18 @@ def multiplicacao(x, y):
 
 def div(x, y):
     x, y = numX, numY
+    if y < 0:
+        print("Impossível dividir por zero!")
     return x/y
 
 
 if choice == '1':
-    print('Você escolheu soma')
-#print(soma(numX, numY))
+    print("Seu resultado é: ", soma(numX, numY))
+elif choice == '2':
+    print("Seu resultado é: ", minus(numX, numY))
+elif choice == '3':
+    print("Seu resultado é: ", multiplicacao(numX, numY))
+elif choice == '4':
+    print("Seu resultado é: ", div(numX, numY))
+else:
+    print("A operação que você escolheu é inválida!")
